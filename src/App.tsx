ed6fd0be7433c44fd2a8d927b93f9a84f08527bd
Igor Viana <igor.viana560@gmail.com>
2025-08-16 18:15:33 -0300

    mudando o css Modules para o styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import EstiloGlobal, { Container } from './style'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
-
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <div className="container">
-        <ListaVagas />
-      </div>
+      <EstiloGlobal />
+      <Container>
+        <Header />
+        <Hero />
+        <div className="container">
+          <ListaVagas />
+        </div>
+      </Container>
     </>
   )
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,15 @@
-import styles from './Hero.module.css'
+import { FormHero, HeroTitle, ContainerHero } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <FormHero>
+    <ContainerHero>
+      <div>
+        <HeroTitle>
+          As melhores vagas para tecnologia, design e artes visuais.
+        </HeroTitle>
+      </div>
+    </ContainerHero>
+  </FormHero>
 )
 
 export default Hero
